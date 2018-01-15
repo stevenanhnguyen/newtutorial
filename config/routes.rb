d9f6a22c518e12548084443b3a496a443d5d0ddf
stevenanhnguyen <at.mrsteven@gmail.com>
2018-01-15 14:40:29 +0700

    Static Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,7 @@
 Rails.application.routes.draw do
+	scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    get  "static_pages/home"
+    get  "static_pages/help"
+    root "application#hello"
+  end
 end
