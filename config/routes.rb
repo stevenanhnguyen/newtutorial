6b7711f60abb3e7fe00c31b48db87f620d43a8df
stevenanhnguyen <at.mrsteven@gmail.com>
2018-01-18 19:04:14 +0700

    filling-in-layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 	scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
-    get  "static_pages/home"
-    get  "static_pages/help"
-    root "application#hello"
+    root "static_pages#home"
+    get "/help", to: "static_pages#help"
+    get "/about", to: "static_pages#about"
+    get "/contact", to: "static_pages#contact"  
   end
 end
