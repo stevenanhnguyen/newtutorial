17789bb7edc926aaf190b74958c10ef682c1792e
stevenanhnguyen <at.mrsteven@gmail.com>
2018-02-08 15:19:09 +0700

    Finished Advanced Login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
+  attr_reader :remember_token
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.{1}[a-z]+\z/i
-  enum sex: %w(male female).freeze
 
   validates :name, presence: true,
     length: {maximum: Settings.users.name.length.maximum}
@@ -13,6 +14,35 @@ class User < ApplicationRecord
   before_save :downcase_email
 
   has_secure_password
+
+  class << self
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    @remember_token = User.new_token
+    update_attributes remember_digest: User.digest(remember_token)
+  end
+
+  def forget
+    update_attributes remember_digest: nil
+  end
+
+  def authenticate? remember_token
+    return false unless remember_digest
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
 
   private
 
